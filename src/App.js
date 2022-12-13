2f122a560a466ed14956c9263840869545fb28fc
kakon-mehedi <kakon9000@gmail.com>
2022-12-13 14:58:04 +0600

    Deploying Fresh React Project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./app.css";
+
+function App() {
+  return (
+    <div className="container">
+      <h1 style={{ textAlign: "center" }} className="heading">
+        Welcome! <br></br>This is a Fresh React
+      </h1>
+    </div>
+  );
+}
+
+export default App;
